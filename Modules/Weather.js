c20b9eeb4893656e4f3f5b6eb9ecb8441971f59a
Alessandro Benetton <aleben98@gmail.com>
2016-07-02 19:48:26 +0200

    Weather

--- Modules/Weather.js
+++ Modules/Weather.js
@@ -2,29 +2,37 @@
  * Created by alebe on 02/07/2016.
  */
 
+forecastDays = 7;
+
+//DAQ Part
+
 //Current Weather DAQ
 
 var cWeather = new XMLHttpRequest();
-cWeather.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + config.city + "&appid=" + config.api +"&units=" + config.unit, false);
+cWeather.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + config.city + "&appid=" + config.api +"&units=" + config.unit + "&lang=" + config.language, false);
 cWeather.send(null);
 var currentWeather = JSON.parse(cWeather.response);
 
 //Weather Forecast DAQ
 
 var wForecast = new XMLHttpRequest();
-wForecast.open("GET", "http://api.openweathermap.org/data/2.5/forecast?q=" + config.city + "&appid=" + config.api +"&units=" + config.unit, false);
+wForecast.open("GET", "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + config.city + "&appid=" + config.api +"&units=" + config.unit + "&lang=" + config.language + "&cnt=" + forecastDays, false);
 wForecast.send(null);
 var weatherForecast = JSON.parse(wForecast.response);
 
+//Output Part
+
 //Prints current weather data
 
-document.write("Current Weather for " + currentWeather.name + "<br />");
-document.write(currentWeather.weather.icon + "<br />");
-document.write("Temp: " + currentWeather.main.temp + "°C" + "<br />");
-document.write("Cluds: " + currentWeather.clouds.all + "<br />");
-document.write("Humidity: " + currentWeather.main.humidity + "<br />" + "<br />" + "<br />");
+document.write("Meteo attuale per " + currentWeather.name + "<br />");
+document.write(currentWeather.weather[0].description + "<br />");
+document.write("Temperatura: " + currentWeather.main.temp + "°C" + "<br />");
+document.write("Nuvole: " + currentWeather.clouds.all + "<br />");
+document.write("Umidità: " + currentWeather.main.humidity + "<br />" + "<br />" + "<br />");
 
 //Prints weather forecast data
 
-document.write("Weather Forecast for " + weatherForecast.city.name);
-document.write("");
+document.write("Previsioni meteo per " + weatherForecast.city.name + "<br />");
+document.write("Domani" + "<br />");
+document.write(weatherForecast.list[0].weather[0].description + "<br />");
+document.write(weatherForecast.list[0].temp.day + "<br />");
--- config.js
+++ config.js
@@ -5,4 +5,5 @@ var config = {
         api: "02742dc10bd38602ed5b5053b6382199",
         unit: "metric",
         city: "Treviso,it",
+        language: "it",
 }
