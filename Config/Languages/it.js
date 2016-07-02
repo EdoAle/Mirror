47b59bbd1b0789828c95d5587b304a5ddc15126c
Alessandro Benetton <aleben98@gmail.com>
2016-07-03 00:26:24 +0200

    ok

---
+++ Config/Languages/it.js
@@ -0,0 +1,6 @@
+
+/**
+ * Created by alebe on 03/07/2016.
+ */
+
+var dayOfTheWeek = ["Domenica", "Luned&igrave;", "Marted&igrave;", "Mercoled&igrave;", "Gioved&igrave;", "Venerd&igrave;", "Sabato"];
