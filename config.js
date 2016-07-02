7af3d26e22a51cf806af0eb3f8f2ec08e2c5aa3b
Alessandro Benetton <aleben98@gmail.com>
2016-07-03 00:27:32 +0200

    ok

--- config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by alebe on 02/07/2016.
- */
-var config = {
-        api: "02742dc10bd38602ed5b5053b6382199",
-        unit: "metric",
-        city: "Treviso,it",
-        language: "it",
-}
