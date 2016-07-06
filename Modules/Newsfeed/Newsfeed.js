811f4f760fb29eebe887ad68ad06dd8b48355f73
Alessandro Benetton <aleben98@gmail.com>
2016-07-06 10:26:23 +0200

    Reload

--- Modules/Newsfeed/Newsfeed.js
+++ Modules/Newsfeed/Newsfeed.js
@@ -1,34 +1,39 @@
 /**
  * Created by alebe on 03/07/2016.
  */
+var ReloadTime = 5000;
 
 //XML send request
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
-  if (xhttp.readyState == 4 && xhttp.status == 200) {
-    myFunction(xhttp);
-  }
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+        myFunction(xhttp);
+    }
 };
 xhttp.open("GET", "http://xml.corriereobjects.it/rss/homepage.xml", true);
 xhttp.send();
 
 //Export the data
 
+var divNews = [""];
+
 function myFunction(xml) {
-  var newsName = 'Corriere della Sera' ;
-  document.getElementById("news-name").innerHTML = newsName; 
-  var i = 0;
-  var f = 0;
-  var xmlDoc = xml.responseXML;
-  var divNews = [""];
-  var x = xmlDoc.getElementsByTagName("item");
-  for (i = 0; i <x.length; i++) {
-    divNews[i] = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
-  }
-  document.getElementById("news-title").innerHTML = divNews[f];
-  f++;
-  var t = setTimeout(myFunction, 500);
+    var newsName = 'Corriere della Sera';
+    document.getElementById("news-name").innerHTML = newsName;
+    var i = 0;
+    var xmlDoc = xml.responseXML;
+    var x = xmlDoc.getElementsByTagName("item");
+    for (i = 0; i < x.length; i++) {
+        divNews[i] = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+    }
 }
 
+var f = 0;
 // Update the title
+function printTitle() {
+    document.getElementById("news-title").innerHTML = divNews[f];
+    f++;
+}
+
+setInterval(printTitle, ReloadTime);
