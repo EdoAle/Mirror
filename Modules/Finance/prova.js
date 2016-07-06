0045414eaa40ab344af6573644fcf71cff1bc741
Edoardo Scarpel <edoardo.scarpel55@gmail.com>
2016-07-06 23:37:45 +0200

    dvcgsvdvcj

--- Modules/Finance/prova.js
+++ Modules/Finance/prova.js
@@ -1,4 +1,5 @@
 
+
 var xmlhttp = new XMLHttpRequest();
 var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22%2C%22UCG.MI%22%2C%22FTSE.MI%22%2C%22EBAY%22%2C%22AMZN%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json";
 
