76fcccd5749c6fd7974013946ffa5a7325f21630
Alessandro Benetton <aleben98@gmail.com>
2016-07-11 12:23:10 +0200

    incasinamento

---
+++ Modules/Newsfeed/ComeIncasinarsiLaVita/Newsfeed.js
@@ -0,0 +1,63 @@
+function newsUpdate2() {
+
+
+//XML send request
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            myFunction(xhttp);
+        }
+    };
+    xhttp.open("GET", "http://xml.corriereobjects.it/rss/homepage.xml", true);
+    xhttp.send();
+
+
+//Export the data
+
+var titleNews = [];
+
+var newnewsDate = [];
+
+var cDate = new Date();
+
+var result = [];
+
+var result1 = [];
+
+var newsName = 'Corriere della Sera';
+
+var newsDate = [];
+
+function myFunction(xml) {
+    var i = 0;
+    var xmlDoc = xml.responseXML;
+    var x = xmlDoc.getElementsByTagName("item");
+    for (i = 0; i < x.length; i++) {
+        divNews.title[i] = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+        newsDate[i] = x[i].getElementsByTagName("pubDate")[0].childNodes[0].nodeValue;
+        newnewsDate[i] = new Date(newsDate[i]);
+        divNews.date[i] = ((cDate - newnewsDate[i])/1000)/60;
+    }
+    newsLength = x.length;
+    bubbleSort();
+    document.getElementById("news-title").innerHTML = divNews.title[0];
+};
+function bubbleSort() {
+      var length = divNews.date.length;
+      for (var i = (length - 1); i >= 0; i--) { //Number of passes
+        for (var j = (length - i); j > 0; j--) {
+          //Compare the adjacent positions
+          if(divNews.date[j] < divNews.date[j-1]) {
+            //Swap the numbers
+            var tmp = divNews.title[j];
+            var tmp2 = divNews.date[j];
+            divNews.title[j] = divNews.title[j-1];
+            divNews.date[j] = divNews.date[j-1];
+            divNews.title[j-1] = tmp;
+            divNews.date[j-1] = tmp2;
+          }
+        }        
+      }
+    }
+};
---
+++ Modules/Newsfeed/ComeIncasinarsiLaVita/Newshour.js
@@ -0,0 +1,80 @@
+function newsUpdate1() {
+
+//XML send request
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+        myFunction(xhttp);
+    }
+};
+xhttp.open("GET", "http://xml.corriereobjects.it/rss/homepage.xml", true);
+xhttp.send();
+
+var newsDate = [""];
+
+var resultDate = [""];
+
+var newnewsDate = [""];
+
+var result = [""];
+
+var currentDate = new Date();
+
+function myFunction(xml) {
+    var i = 0;
+    var xmlDoc = xml.responseXML;
+    var x = xmlDoc.getElementsByTagName("item");
+    for (i = 0; i < x.length; i++) {
+        newsDate[i] = x[i].getElementsByTagName("pubDate")[0].childNodes[0].nodeValue;
+        newnewsDate[i] = new Date(newsDate[i]);
+        result[i] = ((currentDate - newnewsDate[i])/1000)/60;
+        resultDate[i] = Math.round(result[i]);
+    }
+    bubbleSort();
+    setRules(x);
+    document.getElementById("news-name").innerHTML = newsName + ", " + newsHours[0];
+};
+
+function setRules(x){
+    for (i = 0; i < x.length; i++) {
+        if (resultDate[i] >= 1440){
+            newsHours[i] = Math.round((resultDate[i] / 60) / 24);
+            if (newsHours[i] > 1){
+                newsHours[i] = newsHours[i] + " giorni fa:";
+            }else{
+                newsHours[i] = newsHours[i] + " giorno fa:";
+            }
+        }else if (resultDate[i] >= 60){
+            newsHours[i] = Math.round(resultDate[i] / 60);
+            if (newsHours[i] > 1){
+                newsHours[i] = newsHours[i] + " ore fa:";
+            }else{
+                newsHours[i] = newsHours[i] + " ora fa:";
+            }
+        }else{
+            newsHours[i] = resultDate[i];
+            if (newsHours[i] > 1){
+                newsHours[i] = newsHours[i] + " minuti fa:";
+            }else{
+                newsHours[i] = newsHours[i] + " minuto fa:";
+            }
+        }
+    }
+};
+
+function bubbleSort() {
+    var length = resultDate.length;
+    for (var i = (length - 1); i >= 0; i--) { //Number of passes
+        for (var j = (length - i); j > 0; j--) {
+          //Compare the adjacent positions
+            if(resultDate[j] < resultDate[j-1]) {
+            //Swap the numbers
+                var tmp = resultDate[j];
+                resultDate[j] = resultDate[j-1];
+                resultDate[j-1] = tmp;
+            }
+        }        
+    }
+};
+};
