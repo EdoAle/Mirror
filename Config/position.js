21f9d1d201523ca05fdf24feb3b3a135e4f26ab0
Edoardo Scarpel <edoardo.scarpel55@gmail.com>
2016-08-22 16:58:39 +0200

    hbvhbad

--- Config/position.js
+++ Config/position.js
@@ -1,6 +1,28 @@
 var triangulate = require('wifi-triangulate');
- 
-triangulate(function (err, location) {
-  latitude = location.lat;
-  longitude = location.lng;
-})
+var location = 0;
+var item = 0;
+
+//function foo(){
+  //console.log("sono nella funzione");
+  //while (item == 0){
+    //console.log("sono nel loop");
+
+    triangulate(function (err, location){
+      //console.log("sono nella funzione dentro il loop");
+      //console.log(location);
+    document.getElementById("demo").innerHTML = (location.lat);
+      //item = location.lat;
+    });
+    //while(item == 0){
+      //console.log("foo" + item);
+    //}
+    //return(item);
+//}
+/*var ciclo = setInterval(abc, 2000);
+function abc(){
+  location = foo();
+  if (location != 0){
+    clearInterval(ciclo);
+    document.getElementById("demo").innerHTML = (location);
+  }
+}*/
