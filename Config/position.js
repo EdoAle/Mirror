83a9df1cdce6aa1c318b5de2cce3d48296bff923
Edoardo Scarpel <edoardo.scarpel55@gmail.com>
2016-08-22 13:23:00 +0200

    kdjhbkhwberkfb

--- Config/position.js
+++ Config/position.js
@@ -1,9 +1,6 @@
 var triangulate = require('wifi-triangulate');
-var latitude;
-var longitude;
  
 triangulate(function (err, location) {
   latitude = location.lat;
   longitude = location.lng;
 })
-document.getElementById("last-box").innerHTML = "latitudine: " + latitude + "<br /> longitudine: " + latitude; // => { lat: 38.0690894, lng: -122.8069356, accuracy: 42 } 
