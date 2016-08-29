53978b1f6f87ce5002f896c5e1f605933d45ea71
Edoardo Scarpel <edoardo.scarpel55@gmail.com>
2016-08-29 10:47:09 +0200

    hic gv

--- Config/wifi-control.js
+++ Config/wifi-control.js
@@ -1,4 +1,4 @@
-var WiFiControl = require('wifi-control');
+/*var WiFiControl = require('wifi-control');
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -14,11 +14,11 @@ const rl = readline.createInterface({
   WiFiControl.init({
     debug: true
   });
- 
+
   //  Try scanning for access points: 
   WiFiControl.scanForWiFi( function(err, response) {
     if (err) console.log(err);
-    console.log(response);
+    console.log(response.networks[9].ssid);
   });
 
 rl.question("What's your wifi? ", (answer) => {
@@ -30,5 +30,32 @@ rl.question("What's your wifi? ", (answer) => {
       console.log(response);
   });
   });
+});*/
+
+var wifi = require('node-wifi');
+
+//Initialize wifi module
+wifi.init({
+    debug : true,
+    iface : null
+    // the OS will find the right network interface if it is null  
 });
+
+//Scan networks
+wifi.scan(function(err, networks) {
+
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(networks);
+    }
+});
+
+//Connect to a network
+wifi.connect({ ssid : "ssid", password : "password"}, function(err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log('Connected');
+}); 
   
